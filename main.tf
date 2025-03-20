885486ef75311bce4121af74dcd16fe5e32492f7
shanmukh <josyulashanmukh@gmail.com>
2025-03-20 17:33:28 +0530

    this is second commit

--- main.tf
+++ main.tf
@@ -1,1 +1,17 @@
-Hi shanmukh welcome to terraform.
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-04b4f1a9cf54c11d0" 
+  instance_type = "t2.micro"
+}
